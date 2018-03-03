60909cb0b2e2579a7348d8284fed78583241b83b
Brandon Samuel <brandonsamuel72@gmail.com>
2018-03-03 10:09:43 +0000

    added body parser to be able to add a new campground

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+let campgrounds = [
+  {name: "Salmon Creek", image: "https://source.unsplash.com/K9olx8OF36A"},
+  {name: "Granite Hill", image: "https://source.unsplash.com/h4bBVo_CpqQ"},
+  {name: "Mountain Goat Rest", image: "https://source.unsplash.com/yOujaSETXlo"}
+]
 
 
 app.get("/", function(req, res){
   res.render("landing");
-})
+});
+
 
 app.get("/campgrounds", function(req, res){
-  let campgrounds = [
-    {name: "Salmon Creek", image: "https://source.unsplash.com/K9olx8OF36A"},
-    {name: "Granite Hill", image: "https://source.unsplash.com/h4bBVo_CpqQ"},
-    {name: "Mountain Goat Rest", image: "https://source.unsplash.com/yOujaSETXlo"}
-  ]
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+
+app.post("/campgrounds", function(req, res){
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name: name, image: image}
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
 })
 
 
