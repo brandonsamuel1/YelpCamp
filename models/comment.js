b9beb18a41f62e72a6871b75863ebf40a2bb1482
Brandon Samuel <brandonsamuel72@gmail.com>
2018-03-07 02:59:36 +0000

    refactored entire app; put routes in appropriate directories. Also gave users ability to add campgrounds

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const commentSchema = mongoose.Schema({
   text: String,
-  author: String
-})
+  author: {
+    id: {
+        type: mongoose.Schema.Types.ObjectId, ref: "User"
+    },
+  username: String}
+});
 
 module.exports = mongoose.model("Comment", commentSchema);
---
+++ routes/campgrounds.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const Campground = require("../models/campground");
+
+router.get("/", function(req, res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/campgrounds", {campgrounds: allCampgrounds});
+    }
+  })
+});
+
+
+router.post("/", isLoggedIn, function(req, res){
+  let name = req.body.name;
+  let image = req.body.image;
+  let desc = req.body.description;
+  let author = {id: req.user._id, username: req.user.username}
+  let newCampground = {name: name, image: image, description: desc, author: author}
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  })
+});
+
+
+router.get("/new", isLoggedIn, function(req, res){
+  res.render("campgrounds/new");
+});
+
+
+router.get("/:id", function(req, res){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/show", {campground: foundCampground});
+    }
+  })
+});
+
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+};
+
+module.exports = router;
