2c7f0c7acb1d22d91fb469b900febc09b2c526ae
Brandon Samuel <brandonsamuel72@gmail.com>
2018-03-07 08:46:48 +0000

    users are able to update and delete campgrounds and comments that belong to them

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -45,11 +45,58 @@ router.get("/:id", function(req, res){
 });
 
 
+router.get("/:id/edit", ownCampground, function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    res.render("campgrounds/edit", {campground: foundCampground});
+  });
+});
+
+
+router.put("/:id", ownCampground, function(req, res){
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+    if(err){
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+});
+
+
+router.delete("/:id", ownCampground, function(req, res){
+  Campground.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  })
+});
+
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
 };
+
+function ownCampground(req, res, next){
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+        res.redirect("/campgrounds");
+      } else {
+        if(foundCampground.author.id.equals(req.user._id)){
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 
 module.exports = router;
